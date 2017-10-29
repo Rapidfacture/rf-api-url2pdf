9a77ec71cd48ededdab4ab02ed149123c761d4a1
Felix <ff@rapidfacture.com>
2017-10-29 11:52:10 +0100

    add linting, update docs

--- README.md
+++ README.md
@@ -1,37 +1,74 @@
 # rf-api-url2pdf
 
-* unstable - do not use now
-* will be the common part for every rapidfacture app
-
-API url to pdf service.
+* create a pdf from a website via given url
+* plugin for `rf-api`
 
 
 ## Getting Started
 
 > npm install rf-api-url2pdf
 
-## Example
+### Init the service
+
+
 ```js
-// load API
-var API = require("rf-load").require("rf-api").API;
+var Loader = require('rf-load').moduleLoader
+var load = new Loader()
+load.setModulePath(config.paths.modules)
 
-// load module and give saveDir parameter (path where the pdfs are stored)
-require("rf-load").module("rf-api-url2pdf", {saveDir: '/pdfTmp'}); 
+// other stuff
+load.file('db')
+load.file('http')
 
-/** 
-* getPdf: get Pdf from Url
-*
-* @param url: URL string
-* @param res: Express ressource
-* @param callback: (optional) callback function
-* @param getFilePath: (boolean) only get the pdf file path
-*/
-API.ServiceFactory.getPdf(url, res, callback, getFilePath); // execute getPdf function
+// start request api
+load.file('rf-api')
+
+// plug in url2pdf into the api
+load.module("rf-api-url2pdf", {
+   saveDir: '/pdfTmp' // path where the pdfs are stored
+});
+
 ```
 
-## Dependencies
 
-Needs to have url2pdf-plus & rf-api to be installed.
+### Use the service
+```js
+
+// load Services from API
+var Services = require("rf-load").require("rf-api").API.ServiceFactory;
+
+app.get('/pdf', function(req, res){
+
+   var url = 'https://www.ebay.de';
+
+   // @params: respond the pdf to the client
+   Services.getPdf(url, res);
+
+   // @example: optional callback
+   Services.getPdf(url, res, function(pdf){
+      // do sth with the pdf
+   });
+
+   // @example: get only file path
+   var pdfFilePath = Services.getPdf(url, res, null, "getFilePath")
+})
+
+
+```
+
+## Peer Dependencies
+* rf-api
+
+
+## Development
+
+Install the dev tools with
+
+> npm install
+
+Then you can runs some test cases and eslint with:
+
+> npm test
 
 
 ## Legal Issues
--- index.js
+++ index.js
@@ -1,61 +1,58 @@
-/* jshint node: true */ "use strict";
-
 /**
  * API service: url2pdf
  */
 
-var url2pdf = require("url2pdf-plus"),
-	fs = require("fs"),
-	API = require("rf-load").require("rf-api").API;
+var url2pdf = require('url2pdf-plus'),
+   fs = require('fs'),
+   API = require('rf-load').require('rf-api').API
 
-module.exports.start = function(options, next) {
-    /** 
+module.exports.start = function (options, next) {
+   /**
     * getPdf: get Pdf from Url
     *
     * @param url: URL string
-    * @param res: Express ressource
+    * @param res: Express response
     * @param callback: (optional) callback function
     * @param getFilePath: (boolean) only get the pdf file path
     * @example
     * var API = require("rf-load").require("rf-api").API; // load API
     * API.ServiceFactory.getPdf(url, res, callback, getFilePath); // execute getPdf function
     */
-	function getPdf(url, res, callback, getFilePath) { // Export
-	  //Add internal token for unblocking angular js request
-	  url = url + '?internal=ksdf6s80fsa9s0madf7s9df';
-	  url2pdf.renderPdf(url, {
-	      loadTimeout: 2000,
-	      saveDir: options.saveDir,
-	      //debug: true
-	    })
-	    .then(function(filePath) {
-	      //console.log(filePath);
-	      if(getFilePath){  // just return filePath
-	          callback(filePath, res);
-	      }else{ // or return whole file
-	          fs.readFile(filePath, function(err, data) {
-	              if(err){
-	                  res.status(404).send('Server Error, function API.getPdf, readFile ' + filePath + ", "  + err).end();
-	              }else if(callback){
-	                  callback(data, res);
-	              }else{
-	                  // default "send" method converts binary data to a UTF8 string
-	                  //  => pdf modified (not working any longer)
-	                  // prevent this by creating an json array before sending
-	                  res.status(200).send(new Buffer(data, 'binary').toJSON());
-	              }
-	          });
-	      }
-	    })
-	    .catch(function(err) {
-	      res.status(404).send("Error generating pdf, error: " + err);
-	    });
-	};
+   function getPdf (url, res, callback, getFilePath) { // Export
+      // Add internal token for unblocking angular js request
+      url = url + '?internal=ksdf6s80fsa9s0madf7s9df'
+      url2pdf.renderPdf(url, {
+         loadTimeout: 2000,
+         saveDir: options.saveDir
+         // debug: true
+      })
+         .then(function (filePath) {
+            // console.log(filePath);
+            if (getFilePath) { // just return filePath
+               callback(filePath, res)
+            } else { // or return whole file
+               fs.readFile(filePath, function (err, data) {
+                  if (err) {
+                     res.status(404).send('Server Error, function API.getPdf, readFile ' + filePath + ', ' + err).end()
+                  } else if (callback) {
+                     callback(data, res)
+                  } else {
+                     // default "send" method converts binary data to a UTF8 string
+                     //  => pdf modified (not working any longer)
+                     // prevent this by creating an json array before sending
+                     res.status(200).send(new Buffer(data, 'binary').toJSON())
+                  }
+               })
+            }
+         })
+         .catch(function (err) {
+            res.status(404).send('Error generating pdf, error: ' + err)
+         })
+   };
 
-	API.ServiceFactory.registerFunction(getPdf);
+   API.ServiceFactory.registerFunction(getPdf)
 
-	//console.log(API);
+   // console.log(API);
 
-	next();
-
-};
+   next()
+}
